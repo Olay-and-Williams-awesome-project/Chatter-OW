c3f74e2e80b09e6d545ad2f2f44f3a1090bd9f0d
OlayG <ogureje@yahoo.com>
2016-08-04 13:56:30 -0400

    updated model

---
+++ Chatter/Chatter/Models/Chit.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.Globalization;
+
+namespace Chatter.Models
+{
+    public class Chit
+    {
+        public int ChitID { get; set; }
+        public string ChitText { get; set; }
+        [DataType(DataType.DateTime)]
+        public string ChitCreatedAt { get; set; }
+    }
+}
